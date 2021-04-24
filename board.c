28ce7e2f2268366a957c5ebab5364ca37e3cc2ba
Paul John Lee <leesk3@uci.edu>
2021-04-24 00:27:32 -0700

    Update board.c

--- board.c
+++ board.c
@@ -15,7 +15,7 @@ void deleteBoard(PIECE *myBoard){
 }
 
 PIECE *getPiece(PIECE **myBoard, int col, int row){
-	PIECE *p =  &myBoard[row][col];
+	PIECE *p = &myBoard[row][col];
 	return p;
 }
 
@@ -23,7 +23,37 @@ void placePiece(PIECE **myBoard, PIECE *p, int col, int row){
 	myBoard[row][col] = *p;	
 }
 
-void removePiece(PIECE **myBoard, int col, int row){
-
+PIECE removePiece(PIECE **myBoard, int col, int row){
+	PIECE p = myBoard[row][col];
+	PIECE *e = NewPiece(' ', ' ');
+	myBoard[row][col] = *e;
+	return p;
 }
 
+PIECE *movePiece(PIECE** myBoard, int colSource, int rowSource, int colDestination,int rowDestination){
+	PIECE *p = NULL;
+	PIECE *e = NewPiece(' ', ' ');
+	myBoard[rowDestination][colDestination] = myBoard[rowSource][colSource];
+	myBoard[rowSource][colSource] = *e;
+	p = &myBoard[rowDestination][colDestination];
+	return p;
+}
+
+void printBoard(PIECE** myBoard){
+	int i = 0, j = 0;
+	for(i = 0; i < size; i++){
+		for(j = 0; j < size; j++){
+			printf("row: %d col: %d %c%c\n", i, j, GetColor(&myBoard[i][j]), GetType(&myBoard[i][j]));
+		}
+	}
+}
+
+void initializeBoard(PIECE** myBoard){
+	int i = 0, j = 0;
+	PIECE *blank = NewPiece(' ', ' ');
+	for(i = 0; i < size; i++){
+		for(j = 0; j < size; j++){
+			myBoard[i][j] = *blank;
+		}
+	}
+}
