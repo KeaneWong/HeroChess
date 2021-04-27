c9669b9a809e74d54fd95b676f10097b72f59733
Rachel Villamor <rvillam1@uci.edu>
2021-04-27 12:36:10 -0700

    Created Main Alpha Release Directory

---
+++ Chess_Alpha/README.md
@@ -0,0 +1,18 @@
+# HeroChess v1.0
+A chess software program. 
+
+**Update History:**
+
+04/25/2021 - Initial Version
+
+**Authors:**
+* Irania Mazariegos
+* Keane Wong
+* Mario Tafoya 
+* Paul Lee
+* Rachel Villamor
+
+This is the alpha version of the chess software, to install, please type "make" on the command line.
+For more detailed instructions, please refer to the user manual.
+
+Enjoy!
---
+++ Chess_Alpha/src/piece.c
@@ -0,0 +1,31 @@
+#include "piece.h"
+
+PIECE *NewPiece(char type, char color){
+	PIECE *p;
+	p = (PIECE*)malloc(sizeof(PIECE));
+	if (!p)
+	{ 
+		perror("Out of memory! Aborting...");
+		exit(10);
+	} /* fi */
+
+	p->type = type;
+	p->color = color;
+	return p;
+}
+
+PIECE makeBlank(){
+	PIECE p;
+	p.type = ' ';
+	p.color = ' ';
+	return p;
+}
+
+/*return player number*/
+char GetType(PIECE *mypiece){
+	return mypiece->type;
+}
+
+char GetColor(PIECE *mypiece){
+	return mypiece->color;
+}
