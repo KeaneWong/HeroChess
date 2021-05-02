36bae80dd75adff0d2adba29a34f2b953dc30c2b
Rachel Villamor <rvillam1@uci.edu>
2021-05-02 02:55:08 -0700

    Update settings.c

--- Chess_Alpha_src/src/settings.c
+++ Chess_Alpha_src/src/settings.c
@@ -1,83 +1,43 @@
-/* code below is temporary, will push code that actually 
-  works later to avoid conflicts with any updates -rachel */
-/**********************************************************/
+/************************************************************************/
+/* settings.c: player and game settings menu and handling for HeroChess */
+/* 									*/
+/* Modifications:							*/
+/* Date			Author			Description		*/
+/* 2021/05/01		Rachel Villamor		Initial Version		*/
+/************************************************************************/
 
-/* player and game settings menu and handling */
-void settings()
+#include "settings.h"
+
+/* user chooses which playes gets the white pieces */
+int whitepnum(void)
 {
-	int valid_input = 0;
-	int option = 0;
-	int mainmenu = 0;
+	int wp;
+	int done = 0;
 	
-	while (mainmenu == 0)
+	while (done == 0)
 	{
-	    /* Settings Menu */
-    	printf("Welcome to Settings!\n");
-    	printf("1. Change Player Colors\n");
-    	printf("2. Go Back to Main Menu\n");
-    	scanf("%d", &option);
-    	getchar();
-    	
-    	while (valid_input == 0)
-    	{
-    		switch(option)
-    		{
-    			/* variables to store player numbers when user makes customizations */
-    			int wp = 0; 
-    			int bp = 0; 
-    			
-    			case 1:
-    			{
-    			    printf("case 1\n");
-    			    int error = 0;
-    			    
-    			    /* remind user of chess's color rule and ask for the changes */
-    			    printf("Please keep in mind that by the rules of chess, the player with white pieces goes first!\n");
-    			    wp = whitepnum();
-    			    
-    			    /* automatically assign the black player's color */
-    			    
-    			    
-    			    
-    			    
-    			    break;
-    			}
-    			
-    			case 2:
-    			{
-    				printf("case 2\n");
-    				mainmenu = 1;
-    			    break;
-    			}
-    		
-    			default:
-    			{
-    			    printf("Invalid input!\n");
-    			    valid_input = 1;
-    			    break;
-    			}
-    		}
-    	}
-    	
-    	/* if no changes are made,keep default settings */
-    	
+		printf("Enter which player will have the white pieces (1 or 2):");
+		scanf("%d", &wp);
+		getchar();
+		
+		if ( wp == 1 || wp == 2)
+		{
+			printf("Player %d has the white pieces.\n", wp); 
+			done = 1;
+		}
+		else
+		{
+			printf("Invalid input(s), Try Again!\n");
+			done = 0;
+		}
 	}
-}
-
-/* function to set the white player's to 1 or 2 */
-int whitepnum(int wp)
-{
-	printf("Enter which player will have the white pieces (1 or 2):");
-    scanf("%d", &wp);
-    getchar();
-    printf("Player %d ") 
-    
 	return wp;
 }
 
-/* function to set the black player's to 1 or 2 */
-int blackpnum(int bp)
+/* black pieces are automatically assigned based off input from whitepnum() */
+int blackpnum(int wp)
 {
-	wp =
+	int bp;
+	printf("Player %d has the black pieces.\n", wp==1 ? 2:1);
 	return bp;
 }
