ace0dadfe71590fdf8c95e64fe419441fcb0b83a
Rachel Villamor <rvillam1@uci.edu>
2021-05-02 17:26:08 -0700

    Update settings.h

--- Chess_Alpha_src/src/settings.h
+++ Chess_Alpha_src/src/settings.h
@@ -3,6 +3,7 @@
 /*                                                                        */
 /* Modifications:                                                         */
 /* Date         Author                        Description                 */
+/* 2021/05/02   Irania Mazariegos             Added timer functions       */
 /* 2021/05/01   Rachel Villamor               Initial Version             */
 /**************************************************************************/
 
@@ -13,11 +14,26 @@
 #include <stdio.h> 
 #include <string.h> 
 #include <assert.h>
+#include <time.h>
 
+/* player color handling */
 /* user chooses which playes gets the white pieces */
 int whitepnum(void);
 
 /* black pieces are automatically assigned based off input from whitepnum() */
 int blackpnum(int wp);
+
+/* timer handling */
+/* */
+void delay(int ms);
+
+/* */
+int timer(void);
+
+/* */
+int printTime(void);
+
+/* */
+int selection(void);
 
 #endif
