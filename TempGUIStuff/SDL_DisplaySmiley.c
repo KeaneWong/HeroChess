af77963804468c65c922c8d4813c26db80aa59c5
Irania Ruby Mazariegos <imazarie@uci.edu>
2021-05-09 01:35:54 -0700

    Update SDL_DisplaySmiley.c

--- TempGUIStuff/SDL_DisplaySmiley.c
+++ TempGUIStuff/SDL_DisplaySmiley.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 	SDL_Surface *screen;	//This pointer will reference the backbuffer
 	SDL_Surface *image;	//This pointer will reference our bitmap sprite
 	SDL_Surface *temp;	//This pointer will temporarily reference our bitmap sprite
-	SDL_Rect src, dest;	//These rectangles will describe the source and destination regions of our blit
+	SDL_Rect src, pawn, rook, knight, bishop, queen, king;	//These rectangles will describe the source and destination regions of our blit
 	int x;
 #ifdef EVENTS
 	SDL_Event event;	/* Event structure */
@@ -55,20 +55,60 @@ int main(int argc, char *argv[])
 	src.h = image->h;	//Use image->h to display the entire height of the image
 	
 	//Construct the destination rectangle for our blit
-	dest.x = 120;		//Display the image at the (X,Y) coordinates (0,0)
-	dest.y = 120;
-	dest.w = image->w;	//Ensure the destination is large enough for the image's entire width/height
-	dest.h = image->h;
+	pawn.x = 80;		//Display the image at the (X,Y) coordinates (0,0)
+	pawn.y = 80;
+	pawn.w = image->w;	//Ensure the destination is large enough for the image's entire width/height
+	pawn.h = image->h;
 	
 	for(x=0; x<8; x++)	// print 8 smileys at top row
 	{
-		dest.x = x*image->w;
+		pawn.x = x*image->w;
 		//Blit the image to the backbuffer
-		SDL_BlitSurface(image, &src, screen, &dest);
+		SDL_BlitSurface(image, &src, screen, &pawn);
 	
 		//Update the window at the modified region
-		SDL_UpdateRect(screen, dest.x, dest.y, dest.w, dest.h);
+		SDL_UpdateRect(screen, pawn.x, pawn.y, pawn.w, paw .h);
 	}
+	
+	rook.x = 80;
+	rook.y = 0;
+	rook.w = image->w;
+	rook.h = image->h;
+	
+	SDL_BlitSurface(image, &src, screen, &rook);
+	SDL_UpdateRect(screen, rook.x, rook.y, rook.w, rook.h);
+	
+	knight.x = 160;
+	knight.y = 0;
+	knight.w = image->w;
+	knight.h = image->h;
+	
+	SDL_BlitSurface(image, &src, screen, &knight);
+	SDL_Updaterect(screen, knight.x, knight.y, knight.w, knight.h);
+	
+	bishop.x = 240;
+	bishop.y = 0;
+	bishop.w = image->w;
+	bishop.h = image->h;
+	
+	SDL_BlitSurface(image, &src, screen, &bishop);
+	SDL_UpdateRect(screen, bishop.x, bishop.y, bishop.w, bishop.h)
+		
+	queen.x = 320;
+	queen.y = 0;
+	queen.w = image->w;
+	queen.h = image->h;
+	
+	SDL_BlitSurface(image, &src, screen, &queen);
+	SDL_UpdateRect(screen, queen.x, queen.y, queen.w, queen.h);
+	
+	king.x = 400;
+	king.y = 0;
+	king.w = image->w;
+	king.h = image->h;
+	
+	SDL_BlitSurface(image, &src, screen, &king);
+	SDL_UpdateRect(screen, king.x, king.y, king.w, king.h);
 	
 #ifdef WAIT
 	//Wait for 5s so we can see the image
@@ -94,10 +134,10 @@ int main(int argc, char *argv[])
 				printf("Position is x=%d, y=%d.\n",
 					event.button.x, event.button.y);
 				// put a knight there!
-				dest.x = event.button.x;
-				dest.y = event.button.y;
-				SDL_BlitSurface(image, &src, screen, &dest);
-				SDL_UpdateRect(screen, dest.x, dest.y, dest.w, dest.h);
+				pawn.x = event.button.x;
+				pawn.y = event.button.y;
+				SDL_BlitSurface(image, &src, screen, &pawn);
+				SDL_UpdateRect(screen, pawn.x, pawn.y, pawn.w, pawn.h);
 				break;
 			}
 			case SDL_QUIT:
