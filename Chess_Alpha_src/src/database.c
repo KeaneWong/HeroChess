7ed80385281b77f6b5605c68ae69959540f00bfb
Irania <imazarie@uci.edu>
2021-05-24 11:46:20 -0700

    update database.c

--- Chess_Alpha_src/src/database.c
+++ Chess_Alpha_src/src/database.c
@@ -2,45 +2,27 @@
 // currently buggy; won't let password be inputted
 
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-/* typedef struct data
+int appendUser(char username[100], char password[100])
 {
-	char username[100];
-	char password[100];
-}record; */
-
-int main(void)
-{
-	//int i, choice;
-	char username[100];
-	char password[100];
+	//char username[100];
+	//char password[100];
 	FILE *fp1;
-	//char oname[100];
-	//record det;
-	//int recsize;
-	//char c;
-
-	fp1 = fopen("record.txt", "r+");
+	
+	fp1 = fopen("record.txt", "a+");
 	if(fp1 == NULL)
 	{
-		fp1 = fopen("record.txt", "a+");
-		if(fp1 == NULL)
-		{
-			printf("error in opening file: \n");
-			return -1;
-		}
+		printf("error in opening file: \n");
+		return -1;
 	}
 	
-	//recsize = sizeof(det);
-	
-	//fseek(fp1, 0, SEEK_END);
-	printf("Enter username: ");
-	scanf("%[^\n]", det.username);
+	//printf("Enter username: ");
+	//scanf("%s", username);
 	fprintf(fp1, "Username: %s\n", username);
 
-	printf("Enter password: ");
-	scanf("%[^\n]", det.password);
+	//printf("Enter password: ");
+	//scanf("%s", password);
 	fprintf(fp1, "Password: %s\n\n", password);
 
 	fclose(fp1);
