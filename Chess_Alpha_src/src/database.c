556c2b734de6d8efd6cf79a1626c3875c324b97a
Irania <imazarie@uci.edu>
2021-05-24 11:31:41 -0700

    update database.c

--- Chess_Alpha_src/src/database.c
+++ Chess_Alpha_src/src/database.c
@@ -4,25 +4,27 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct data
+/* typedef struct data
 {
 	char username[100];
 	char password[100];
-}record;
+}record; */
 
 int main(void)
 {
 	//int i, choice;
+	char username[100];
+	char password[100];
 	FILE *fp1;
-	char oname[100];
-	record det;
+	//char oname[100];
+	//record det;
 	//int recsize;
 	//char c;
 
 	fp1 = fopen("record.txt", "r+");
 	if(fp1 == NULL)
 	{
-		fp1 = fopen("record.txt", "w+");
+		fp1 = fopen("record.txt", "a+");
 		if(fp1 == NULL)
 		{
 			printf("error in opening file: \n");
@@ -30,12 +32,17 @@ int main(void)
 		}
 	}
 	
-	recsize = sizeof(det);
+	//recsize = sizeof(det);
 	
-	fseek(fp1, 0, SEEK_END);
+	//fseek(fp1, 0, SEEK_END);
 	printf("Enter username: ");
 	scanf("%[^\n]", det.username);
+	fprintf(fp1, "Username: %s\n", username);
+
 	printf("Enter password: ");
 	scanf("%[^\n]", det.password);
-	fwrite(&det, recsize, 1, fp1);
+	fprintf(fp1, "Password: %s\n\n", password);
+
+	fclose(fp1);
+	return 0;
 }
