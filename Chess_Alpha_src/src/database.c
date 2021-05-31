d5eb3059eea2c4b0f44d4d4ed0888a3391033222
Irania <imazarie@uci.edu>
2021-05-31 02:37:47 -0700

    Update database.c

--- Chess_Alpha_src/src/database.c
+++ Chess_Alpha_src/src/database.c
@@ -3,6 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int appendUser(char username[100], char password[100]);
+int checkUser(char user[100], char pass[100]);
 
 int appendUser(char username[100], char password[100])
 {
@@ -28,3 +32,66 @@ int appendUser(char username[100], char password[100])
 	fclose(fp1);
 	return 0;
 }
+
+int checkUser(char user[100], char pass[100])
+{
+	char line[301];
+	int found = 0;
+
+	FILE *fp1 = fopen("record.txt", "r");
+	if (fp1 == NULL)
+	{
+		printf("Error! File missing\n");
+		exit (10);
+	}
+
+        while(!feof(fp1))
+        {
+                fgets(line, 300, fp1);
+		
+		if(strncmp(user, line, strlen(user)) == 0)
+		{
+			if (strncmp(pass, line, strlen(pass)) == 0)
+			{
+				printf("User exists");
+				found = 1;
+			}
+		
+			else 
+			{
+				printf("Password is incorrect");
+			}
+		}
+	}
+	
+	printf("User does not exist");
+	return found;
+	
+	fclose(fp1);
+}
+
+/* int changePass(char username[100], char newPass[100])
+{
+	char oldPass[100];
+	int found;
+	
+	FILE *fp1 = fopen("record.txt", "r");
+	if (fp1 == NULL)
+	{
+		printf("Error! File missing\n");
+		exit (10);
+	}
+
+        while(!feof(fp1))
+        {
+                fgets(line, 300, fp1);
+		
+		if(strncmp(user, line, strlen(user)) == 0)
+		{
+			
+		}
+		
+	} 
+} */
+
+// EOF //
